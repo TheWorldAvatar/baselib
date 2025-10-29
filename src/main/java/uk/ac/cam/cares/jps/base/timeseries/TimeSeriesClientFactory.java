99537c9e884499ade23e994daba2e933c1a55f2d
Kok Foong Lee <kfl31@cam.ac.uk>
2025-10-29 13:39:13 +0800

    54-time-series-for-ontop: added factory methods to parse timestamp

--- src/main/java/uk/ac/cam/cares/jps/base/timeseries/TimeSeriesClientFactory.java
+++ src/main/java/uk/ac/cam/cares/jps/base/timeseries/TimeSeriesClientFactory.java
@@ -2,6 +2,9 @@ package uk.ac.cam.cares.jps.base.timeseries;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -62,6 +65,28 @@ public class TimeSeriesClientFactory {
             throws ClassNotFoundException, NoSuchMethodException, SecurityException, InstantiationException,
             IllegalAccessException, IllegalArgumentException, InvocationTargetException {
         return getInstance(endpoints, new RemoteStoreClient(), dataIriList);
+    }
+
+    public static Object timestampFactory(String className, String timeString) {
+        return timestampFactory(className, Arrays.asList(timeString));
+    }
+
+    /**
+     * this was initially written so that timestamps in python can be parsed more
+     * efficiently
+     */
+    public static List<Object> timestampFactory(String className, List<String> timeStringList) {
+        List<Object> parsedTime = new ArrayList<>();
+        try {
+            Class<?> clazz = Class.forName(className);
+            Method method = clazz.getMethod("parse", CharSequence.class);
+            for (String timeString : timeStringList) {
+                parsedTime.add(method.invoke(null, timeString));
+            }
+        } catch (Exception e) {
+            throw new JPSRuntimeException("Failed to parse timestamp for " + className, e);
+        }
+        return parsedTime;
     }
 
     private static TimeSeriesClient<?> getInstance(List<String> endpoints, TripleStoreClientInterface storeClient,
